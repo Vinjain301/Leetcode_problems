160135f543df33c0b212c7fef7f5b303d0ff7dac
umang jain <168884538+Vinjain301@users.noreply.github.com>
2025-04-16 19:11:37 -0500

    Time: 266 ms (93.66%), Space: 67 MB (70.89%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    ordered_customer_ids=orders['customerId']
-    return customers.query('id not in @ ordered_customer_ids')[['name']].rename(columns={'name':'Customers'})
+    return pd.DataFrame(customers[~customers.id.isin(orders['customerId'])]['name'].rename('Customers'))
     
