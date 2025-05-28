cb0c35b997f315ae7352b0f53f722f90d8ed44c9
umang jain <168884538+Vinjain301@users.noreply.github.com>
2025-05-28 18:58:24 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (83.15%) - LeetSync

--- Pandas/30_days_of_Pandas/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ Pandas/30_days_of_Pandas/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,35 +1,22 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = []
-        i = j = 0
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                merged.append(nums1[i])
-                i += 1
-            elif nums1[i] == nums2[j]:
-                merged.append(nums1[i])
-                merged.append(nums2[j])
-                i += 1
-                j += 1
+        if len(nums1)>len(nums2):
+            nums1,nums2=nums2,nums1
+        x,y=len(nums1),len(nums2)
+        low,high=0,x
+        while low<=high:
+            partitionx=(low+high)//2
+            partitiony=(x+y+1)//2-partitionx
+            maxleftx=float(-inf) if partitionx==0 else nums1[partitionx-1]
+            minrightx=float(inf) if partitionx==x else nums1[partitionx]
+            maxlefty=float(-inf) if partitiony==0 else nums2[partitiony-1]
+            minrighty=float(inf) if partitiony==y else nums2[partitiony]
+            if maxleftx<=minrighty and maxlefty<=minrightx:
+                if (x+y)%2==0:
+                    return ((max(maxleftx,maxlefty)+min(minrightx,minrighty))/2)
+                else:
+                    return max(maxleftx,maxlefty)
+            elif maxleftx>minrighty:
+                high=partitionx-1
             else:
-                merged.append(nums2[j])
-                j += 1
-        if j < len(nums2):
-            merged.extend(nums2[j:])
-        if i < len(nums1):
-            merged.extend(nums1[i:])
-        if len(merged) % 2 == 0:
-            med = (merged[len(merged) // 2 - 1] + merged[len(merged) // 2]) / 2
-        else:
-            med = merged[len(merged) // 2]
-        return med
-
-
-
-
-            
-        
-
-        
-
-        
+                low=partitionx+1
